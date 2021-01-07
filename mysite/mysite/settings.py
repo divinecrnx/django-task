94292cf8db6592431d8d23716c1d0afe3974d32b
divinecrnx <zulhilmi.rasheed98@gmail.com>
2021-01-07 14:33:45 +0800

    Prototyped signup view

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'index.apps.IndexConfig',
+    'signup.apps.SignupConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
