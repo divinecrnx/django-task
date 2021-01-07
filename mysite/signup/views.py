8f341661771c1c91c58c87b70db8e6eace7e2bfc
divinecrnx <zulhilmi.rasheed98@gmail.com>
2021-01-07 22:14:21 +0800

    Removed old User model; now using django auth Ignored __pycache__

--- mysite/signup/views.py
+++ mysite/signup/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render
 from .forms import UserSignupForm
-from .models import User
 from django.http import HttpResponseRedirect
 from django.urls import reverse
 
@@ -16,9 +15,6 @@ def index(request):
             username = form.cleaned_data['username']
             email = form.cleaned_data['email']
             password = form.cleaned_data['password']
-
-            new_user = User(username=username, email=email, password=password)
-            new_user.save()
 
             return HttpResponseRedirect(reverse('index:index'))
     else:
