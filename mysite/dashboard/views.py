ded16ba90717fd0913d99fe754824e39ca546636
divinecrnx <zulhilmi.rasheed98@gmail.com>
2021-01-10 19:07:56 +0800

    Added non_field_errors to manage file forms Reworked and fixed manage view error displays

--- mysite/dashboard/views.py
+++ mysite/dashboard/views.py
@@ -75,47 +75,83 @@ def submit(request):
 def manage(request, claim_id):
 
     data = Claim.objects.get(id=claim_id)
+
+    get_form = ClaimForm(model_to_dict(data), use_required_attribute=False)
+    get_form_photo = ManageClaimPhotoForm()
+    get_form_ins = ManageClaimInsForm()
+
+    context = {
+        'form': get_form,
+        'form_photo': get_form_photo,
+        'form_ins': get_form_ins,
+        'f_type': 'Manage',
+        'cid': claim_id,
+        'link': data,
+    }
     
     if request.method == 'POST':
-        form = ManageClaimForm(request.POST, use_required_attribute=False)
-        form_photo = ManageClaimPhotoForm(request.POST, request.FILES, use_required_attribute=False)
-        form_ins = ManageClaimInsForm(request.POST, request.FILES, use_required_attribute=False)
+        post_form = ManageClaimForm(request.POST)
+        post_form_photo = ManageClaimPhotoForm(request.POST, request.FILES)
+        post_form_ins = ManageClaimInsForm(request.POST, request.FILES)
 
-        if form.is_valid():
-            data.fullname = form.cleaned_data['fullname']
-            data.email = form.cleaned_data['email']
-            data.mobile_num = form.cleaned_data['mobile_num']
-            data.vehicle_model_year = form.cleaned_data['vehicle_model_year']
-            data.vehicle_model = form.cleaned_data['vehicle_model']
-            data.vehicle_no = form.cleaned_data['vehicle_no']
-            data.accident_dt = form.cleaned_data['accident_dt']
-            data.location = form.cleaned_data['location']
-            data.loss_type = form.cleaned_data['loss_type']
-            data.loss_desc = form.cleaned_data['loss_desc']
-            data.pr_lodged_inq = form.cleaned_data['pr_lodged_inq']
-            data.inq_injury = form.cleaned_data['inq_injury']
-        elif form_photo.is_valid():
+        if post_form.is_valid():
+            data.fullname = post_form.cleaned_data['fullname']
+            data.email = post_form.cleaned_data['email']
+            data.mobile_num = post_form.cleaned_data['mobile_num']
+            data.vehicle_model_year = post_form.cleaned_data['vehicle_model_year']
+            data.vehicle_model = post_form.cleaned_data['vehicle_model']
+            data.vehicle_no = post_form.cleaned_data['vehicle_no']
+            data.accident_dt = post_form.cleaned_data['accident_dt']
+            data.location = post_form.cleaned_data['location']
+            data.loss_type = post_form.cleaned_data['loss_type']
+            data.loss_desc = post_form.cleaned_data['loss_desc']
+            data.pr_lodged_inq = post_form.cleaned_data['pr_lodged_inq']
+            data.inq_injury = post_form.cleaned_data['inq_injury']
+
+            messages.success(request, 'Claim updated!')
+            data.save()
+
+            return redirect(reverse('dashboard:manage', args=(claim_id,)))
+        else:
+            if not len(post_form.data) == 1:
+                context['form'] = post_form
+                print("form ran")
+                return render(request, 'dashboard/manage.html', context)
+
+        if post_form_photo.is_valid():
             data.photo.delete()
-            data.photo = form_photo.cleaned_data['photo']
-        elif form_ins.is_valid():
+            data.photo = post_form_photo.cleaned_data['photo']
+            
+            messages.success(request, 'Claim updated!')
+            data.save()
+
+            return redirect(reverse('dashboard:manage', args=(claim_id,)))
+        else:
+            p_error = post_form_photo.errors.get_json_data()['photo'][0]['message']
+
+            if not p_error == 'This field is required.':
+                context['form_photo'] = post_form_photo
+                return render(request, 'dashboard/manage.html', context)
+
+        if post_form_ins.is_valid():
             data.insurance_cover_note_pdf.delete()
-            data.insurance_cover_note_pdf = form_ins.cleaned_data['insurance_cover_note_pdf']
-        
-        data.save()
+            data.insurance_cover_note_pdf = post_form_ins.cleaned_data['insurance_cover_note_pdf']
+            
+            messages.success(request, 'Claim updated!')
+            data.save()
 
-        messages.success(request, 'Claim updated!')
-        
-        return redirect(reverse('dashboard:manage', args=(claim_id,)))
+            return redirect(reverse('dashboard:manage', args=(claim_id,)))
+        else:
+            i_error = post_form_ins.errors.get_json_data()['insurance_cover_note_pdf'][0]['message']
 
+            if not i_error == 'This field is required.':
+                context['form_ins'] = post_form_ins
+                return render(request, 'dashboard/manage.html', context)
     else:
-        
         if data.status == 'Accepted':
             return render(request, 'dashboard/view_claim.html', {'claim': data})
         else:
-            form = ClaimForm(model_to_dict(data), use_required_attribute=False)
-            form_photo = ManageClaimPhotoForm()
-            form_ins = ManageClaimInsForm()
-            return render(request, 'dashboard/manage.html', {'form': form, 'form_photo': form_photo, 'form_ins': form_ins, 'f_type': 'Manage', 'cid': claim_id, 'link': data})
+            return render(request, 'dashboard/manage.html', context)
 
 @login_required
 def delete(request, claim_id):
