a36bb1ff2a540fc6de2bec442b1316a73defed6b
divinecrnx <zulhilmi.rasheed98@gmail.com>
2021-01-11 11:54:02 +0800

    Added delete claim model test

--- mysite/dashboard/tests.py
+++ mysite/dashboard/tests.py
@@ -3,7 +3,7 @@ from django.core.files.uploadedfile import SimpleUploadedFile
 from django.urls import reverse
 from .models import Claim
 from django.contrib.auth.models import User
-import datetime
+import datetime, pytz
 
 # Create your tests here.
 
@@ -13,6 +13,8 @@ class DashboardViewTests(TestCase):
         self.client.force_login(User.objects.create_user(username='upa', password='1234'))
 
     def create_claim(self):
+        tz = pytz.timezone('Asia/Kuala_Lumpur')
+
         return Claim.objects.create(
             user = User.objects.get(username='upa'),
             fullname='fullname',
@@ -21,7 +23,7 @@ class DashboardViewTests(TestCase):
             vehicle_model_year='MY2020',
             vehicle_model='Proton Persona',
             vehicle_no='ABC 123',
-            accident_dt=datetime.datetime(2021, 1, 1, 16, 0, 0, tzinfo=datetime.timezone.utc),
+            accident_dt=datetime.datetime(2021, 1, 1, 16, 0, 0, tzinfo=tz),
             location='Kuala Lumpur',
             loss_type='OD',
             loss_desc='Some desc',
@@ -35,6 +37,10 @@ class DashboardViewTests(TestCase):
         edit_claim = self.create_claim()
         edit_claim.vehicle_no = 'DEF 456'
         edit_claim.save()
+
+    def delete_claim(self):
+        del_claim = self.create_claim()
+        del_claim.delete()
     
     def test_login_requirement(self):
         response = self.client.get(reverse('dashboard:index'))
@@ -64,3 +70,14 @@ class DashboardViewTests(TestCase):
             response.context['claims'],
             ['<Claim: DEF 456 - Proton Persona>']
         )
+
+    def test_delete_claim(self):
+        x = self.create_claim()
+        response = self.client.get(reverse('dashboard:index'))
+        self.assertQuerysetEqual(
+            response.context['claims'],
+            ['<Claim: ABC 123 - Proton Persona>']
+        )
+        x.delete()
+        response = self.client.get(reverse('dashboard:index'))
+        self.assertQuerysetEqual(response.context['claims'], [])
